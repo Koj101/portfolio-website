1eb23bf9039c1bbf265f53cf71bd85bfc8de67bd
Kojo Tenkorang <ktenkorang2@huskers.unl.edu>
2026-06-07 11:39:10 -0500

    Created Boilerplate React Code

---
+++ my portfolio/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+  return(
+    <>
+    <h1>Home</h1>
+    <h1>About</h1>
+    <h1>Skills</h1>
+    <h1>Projects</h1>
+    <h1>Contact</h1>
+    </>
+  )
+
+
+}
+
+
+export default Header
