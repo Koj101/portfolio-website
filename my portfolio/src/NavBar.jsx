7dcd62925d8d91357e1bc1d9db9fba1a9f52d332
Koj101 <netk4t@gmail.com>
2026-06-11 15:32:38 -0500

    Added "Contact Button to NavBar component

--- my portfolio/src/NavBar.jsx
+++ my portfolio/src/NavBar.jsx
@@ -8,12 +8,15 @@ function NavBar() {
         size={32}
         class="my-2 mx-4  text-sky-950 cursor-pointer hover:text-amber-50"
       />
-      <div class=" flex flex-row mx-auto px-16 ">
+      <div class=" flex items-center flex-row mx-auto px-16 ">
         <NavText text="Home" />
         <NavText text="About" />
         <NavText text="Skills" />
         <NavText text="Project" />
         <NavText text="About" />
+        <button class="mx-5 p-1.5 text-xl text-blue-50 font-bold hover:mx-3 hover:text-sky-100 hover:p-2 bg-blue-500 border-2 border-blue-400 shadow-sx rounded-2xl transition-all ease-in-out hover:bg-blue-800 cursor-pointer">
+          Contact
+        </button>
       </div>
     </div>
   );
