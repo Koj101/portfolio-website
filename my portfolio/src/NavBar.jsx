3b24557a57e15b490c4b77199f4884abbed22710
Koj101 <netk4t@gmail.com>
2026-06-12 02:05:18 -0500

    Implemented page Scrolling for completed Components

--- my portfolio/src/NavBar.jsx
+++ my portfolio/src/NavBar.jsx
@@ -9,9 +9,9 @@ function NavBar() {
         class="my-2 mx-4  text-sky-950 cursor-pointer hover:text-amber-50"
       />
       <div class=" flex items-center flex-row mx-auto px-16 ">
-        <NavText text="Home" />
-        <NavText text="About" />
-        <NavText text="Skills" />
+        <NavText text="Home" scroll={0} />
+        <NavText text="About" scroll={330} />
+        <NavText text="Skills" scroll={530} />
         <NavText text="Projects" />
 
         <button class="mx-16 p-1.5 text-xl text-blue-50 font-bold  hover:text-sky-100 hover:p-2 bg-blue-500 border-2 border-blue-400 shadow-sx rounded-2xl transition-all ease-in-out hover:bg-blue-800 hover:rounded-sm hover:border-blue-800 cursor-pointer">
@@ -21,9 +21,20 @@ function NavBar() {
     </div>
   );
 }
+//Scrolls to pre-determined coordinate on page
+const NavScroll = (scroll) => {
+  window.scrollTo({
+    top: scroll,
+    left: 0,
+    behavior: "smooth",
+  });
+};
 
-const NavText = ({ text }) => (
-  <p class="mx-2 text-xl font-bold hover:mx-3 hover:text-2xl hover:text-blue-900 transition-all ease-in-out cursor-pointer">
+const NavText = ({ text, scroll = 0 }) => (
+  <p
+    onClick={() => NavScroll(scroll)}
+    class="mx-2 text-xl font-bold hover:mx-3 hover:text-2xl hover:text-blue-900 transition-all ease-in-out cursor-pointer"
+  >
     {text}
   </p>
 );
