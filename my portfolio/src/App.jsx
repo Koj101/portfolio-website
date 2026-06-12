d017ddea03c7fdfc07fae311dc569fb1823079f0
Koj101 <netk4t@gmail.com>
2026-06-11 23:30:56 -0500

    Created Bio component

--- my portfolio/src/App.jsx
+++ my portfolio/src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./NavBar";
+import Bio from "./Bio";
 function App() {
   return (
-    <>
+    <div class="flex flex-col items-center">
       <NavBar />
-    </>
+      <Bio />
+    </div>
   );
 }
 
---
+++ my portfolio/src/Bio.jsx
@@ -0,0 +1,14 @@
+function Bio() {
+  return (
+    <div class=" flex justify-center flex-col m-4 w-4xl h-auto p-4 mt-32 shadow-xl shadow-gray-400/25 bg-blue-50 rounded-b-xl border-2 border-gray-400">
+      <p class="text-2xl">Hi I'm</p>
+      <h1 class="text-8xl p-4 font-semibold">Kojo Tenkorang</h1>
+      <p class="text-2xl">Frontend Software Engineer</p>
+      <p class="text-xl mt-2">
+        I Love Designing and Programming Dynamic Front ends for Application
+      </p>
+    </div>
+  );
+}
+
+export default Bio;
