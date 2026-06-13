322f8ab7edba250e7d519aaebcbbda5d2a4df0ce
Koj101 <netk4t@gmail.com>
2026-06-12 23:04:51 -0500

    Updated Text size of About

--- my portfolio/src/About.jsx
+++ my portfolio/src/About.jsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div class="flex flex-col m-4 w-4xl h-auto p-4">
       <h1 class="text-2xl font-bold">About</h1>
-      <p class="mt-2 mb-4">
+      <p class="mt-2 mb-4 text-xl">
         I am currently a undergrad student at the University of Nebraska-Lincoln
         majoring in software engineering. I am personally studying to find ways
         to create websites with creative but functional and practical designs.
