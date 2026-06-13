09eaef17e7fc34f8fa402a3757bbd413d9a9434f
Koj101 <netk4t@gmail.com>
2026-06-12 23:04:26 -0500

    Updated Skills Icon Componets with appriate icons and names

--- my portfolio/src/Skills.jsx
+++ my portfolio/src/Skills.jsx
@@ -1,4 +1,15 @@
-import { FaJs } from "react-icons/fa";
+import {
+  FaJs,
+  FaBootstrap,
+  FaReact,
+  FaJava,
+  FaPython,
+  FaGithub,
+  FaFigma,
+} from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaC } from "react-icons/fa6";
+
 import Icon from "./Icon";
 function Skills() {
   return (
@@ -6,14 +17,14 @@ function Skills() {
       <h1 class="text-2xl font-bold ">Skills</h1>
       <div class="grid grid-cols-3 gap-1 place-items-center w-lg p-2 ">
         <Icon icon={<FaJs size={32} />} name="JavaScript" />
-        <Icon icon={<FaJs size={32} />} name="JavaScript" />
-        <Icon icon={<FaJs size={32} />} name="JavaScript" />
-        <Icon icon={<FaJs size={32} />} name="JavaScript" />
-        <Icon icon={<FaJs size={32} />} name="JavaScript" />
-        <Icon icon={<FaJs size={32} />} name="JavaScript" />
-        <Icon icon={<FaJs size={32} />} name="JavaScript" />
-        <Icon icon={<FaJs size={32} />} name="JavaScript" />
-        <Icon icon={<FaJs size={32} />} name="JavaScript" />
+        <Icon icon={<FaReact size={32} />} name="React" />
+        <Icon icon={<RiTailwindCssFill size={32} />} name="Tailwind" />
+        <Icon icon={<FaBootstrap size={32} />} name="Bootstrap" />
+        <Icon icon={<FaJava size={32} />} name="Java" />
+        <Icon icon={<FaPython size={32} />} name="Python" />
+        <Icon icon={<FaGithub size={32} />} name="github" />
+        <Icon icon={<FaFigma size={32} />} name="Figma" />
+        <Icon icon={<FaC size={32} />} name="C" />
       </div>
     </div>
   );
